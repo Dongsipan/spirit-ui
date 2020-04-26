42f740723a65d6653d0ddc61240dee88103f1254
dongsipan <75098051@qq.com>
2020-04-26 13:29:38 +0800

    use reboot & add new variables

--- src/components/color/color.tsx
+++ src/components/color/color.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 function Color() {
   return (
     <>
-      <div className="color-pannel">
+      <div className="color-panel">
         {
           [5, 4, 3, 2, 1].map(i => (
             <div className={`color-primary-light-${i}`}>
@@ -21,14 +21,14 @@ function Color() {
           ))
         }
       </div>
-      <div className="color-pannel">
+      <div className="color-panel">
         {
           [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].map(i => (
             <div className={`color-neutral-${i}`}>neutral-{i}</div>
           ))
         }
       </div>
-      <div className="color-pannel">
+      <div className="color-panel">
         <div className="color-success">success</div>
         <div className="color-warning">warning</div>
         <div className="color-error">error</div>
