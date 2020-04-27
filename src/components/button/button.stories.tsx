95c011fd453694e792d946debe0d12be90c91f4c
dongsipan <75098051@qq.com>
2020-04-27 19:05:44 +0800

    优化variables

--- src/components/button/button.stories.tsx
+++ src/components/button/button.stories.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Button from './button'
 import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions'
 
 const types = () => (
   <>
-    <Button>默认按钮</Button>
-    <Button btnType="primary">默认按钮</Button>
+    <Button onClick={action('default button clicked')}>默认按钮</Button>
+    <Button btnType="primary" onClick={action('primary button clicked')}>主要按钮</Button>
+    <Button btnType="success" onClick={action('success button clicked')}>成功按钮</Button>
+    <Button btnType="warning" onClick={action('warning button clicked')}>警告按钮</Button>
+    <Button btnType="danger" onClick={action('danger button clicked')}>危险按钮</Button>
   </>
 )
 
--- src/components/color/color.tsx
+++ src/components/color/color.tsx
@@ -3,9 +3,15 @@ function Color() {
   return (
     <>
       <div className="color-panel">
+        <div className="color-primary">primary</div>
+        <div className="color-success">success</div>
+        <div className="color-warning">warning</div>
+        <div className="color-danger">error</div>
+      </div>
+      <div className="color-panel">
         {
           [5, 4, 3, 2, 1].map(i => (
-            <div className={`color-primary-light-${i}`}>
+            <div className={`color-primary-light-${i}`} key={`primary-light-${i}`}>
               light-{i}
             </div>
           ))
@@ -15,7 +21,64 @@ function Color() {
         </div>
         {
           [1, 2, 3, 4, 5].map(i => (
-            <div className={`color-primary-dark-${i}`}>
+            <div className={`color-primary-dark-${i}`} key={`primary-dark-${i}`}>
+              dark-{i}
+            </div>
+          ))
+        }
+      </div>
+      <div className="color-panel">
+        {
+          [5, 4, 3, 2, 1].map(i => (
+            <div className={`color-success-light-${i}`} key={`success-light-${i}`}>
+              light-{i}
+            </div>
+          ))
+        }
+        <div className={`color-success`}>
+          success
+        </div>
+        {
+          [1, 2, 3, 4, 5].map(i => (
+            <div className={`color-success-dark-${i}`} key={`success-dark-${i}`}>
+              dark-{i}
+            </div>
+          ))
+        }
+      </div>
+      <div className="color-panel">
+        {
+          [5, 4, 3, 2, 1].map(i => (
+            <div className={`color-warning-light-${i}`} key={`warning-light-${i}`}>
+              light-{i}
+            </div>
+          ))
+        }
+        <div className={`color-warning`}>
+          warning
+        </div>
+        {
+          [1, 2, 3, 4, 5].map(i => (
+            <div className={`color-warning-dark-${i}`} key={`warning-dark-${i}`}>
+              dark-{i}
+            </div>
+          ))
+        }
+      </div>
+      <div className="color-panel">
+        {
+          [5, 4, 3, 2, 1].map(i => (
+            <div className={`color-danger-light-${i}`} key={`danger-light-${i}`}>
+              light-{i}
+            </div>
+          ))
+        }
+        <div className={`color-danger`}>
+          danger
+        </div>
+        {
+          [1, 2, 3, 4, 5].map(i => (
+            <div className={`color-danger-dark-${i}`} key={`danger-dark-${i}`}>
               dark-{i}
             </div>
           ))
@@ -24,14 +87,9 @@ function Color() {
       <div className="color-panel">
         {
           [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].map(i => (
-            <div className={`color-neutral-${i}`}>neutral-{i}</div>
+            <div className={`color-neutral-${i}`} key={`neutral-${i}`}>neutral-{i}</div>
           ))
         }
-      </div>
-      <div className="color-panel">
-        <div className="color-success">success</div>
-        <div className="color-warning">warning</div>
-        <div className="color-error">error</div>
       </div>
     </>
   );
